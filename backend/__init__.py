cb7908a07ba92b69544422be8abaaab07e524861
DNBao13170 <126451417+Ricky13170@users.noreply.github.com>
2026-02-14 00:33:37 +0700

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,3 +2,5 @@ from .config import *
 from .asr import WhisperASR, StreamingASRProcessor
 from .handler import ASRService, ASRSession
 from .translation import NLLBTranslator
+from .bartpho_corrector import BARTphoCorrector
+from .groq_service import GroqService
